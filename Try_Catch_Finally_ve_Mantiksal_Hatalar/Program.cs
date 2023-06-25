58c399c642f4d7ab2bab81c3ccbb92a23520df7a
Can Ceylan <bilgi@canceylan.com>
2023-06-25 18:35:12 +0300

    1. bölüm tamamlandı

--- Try_Catch_Finally_ve_Mantiksal_Hatalar/Program.cs
+++ Try_Catch_Finally_ve_Mantiksal_Hatalar/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+    //    try
+    //    {
+    //    Console.WriteLine("Bir sayı giriniz: ");
+    //    int sayi = Convert.ToInt32(Console.ReadLine());
+    //    Console.WriteLine("Girmiş olduğunuz sayı: " + sayi);
+    //    }
+    //    catch(Exception ex)
+    //    {
+    //        Console.WriteLine("Hata: " + ex.Message.ToString());
+//
+    //    }
+//
+    //    // finally (* optional)
+    //    finally 
+    //    {
+    //        Console.WriteLine("İşlem tamamlandı.");
+    //    }
+
+        try 
+        {
+            //int a = int.Parse(null);
+            //int a = int.Parse("Test");
+            int a = int.Parse("-200000000000");
+
+        }
+        catch(ArgumentNullException ex)  
+        {
+            Console.WriteLine("Boş değer girdiniz.");
+            Console.WriteLine(ex);
+        }
+
+        catch(FormatException ex)  
+        {
+            Console.WriteLine("Veri tipi uygun değil.");
+            Console.WriteLine(ex);
+        }
+        
+        catch(OverflowException ex)  
+        {
+            Console.WriteLine("Çok küçük veya çok büyük bir değer girdiniz.");
+            Console.WriteLine(ex);
+        }
+        finally 
+        {
+            Console.WriteLine("İşlem başarıyla tamamlandı.");
+        }
+
+
+    
+    
+    
+    
+    
+    
+    }
+}
